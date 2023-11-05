b6c0336bbede7152219dea2f49847c4bd29b3c51
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-11-05 16:40:47 +0545

    refactor: Remove unnecessary imports and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Box } from "ink";
 import React from "react";
-import { CommandInfo, Header, PanesContainer } from "@components/index.js";
+import { CommandInfo, PanesContainer } from "@components/index.js";
 import { BorderColor } from "./types.js";
 import { Versions, Info, Plugins } from "@panes/index.js";
 
 export default function App() {
 	return (
 		<>
-			<Header />
 			<PanesContainer borderColor={BorderColor.UNFOCUSED}>
 				<Box flexDirection="column" width="50%">
 					<Info />
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from "./CommandInfo.js";
 export * from "./PanesContainer.js";
 export * from "./Loader.js";
-export * from "./Header.js";
 export * from "./Title.js";
 export * from "./CustomSelectItem.js";
 export * from "./CustomAlert.js";
