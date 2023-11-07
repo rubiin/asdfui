8caacb12ebcd34e3a5c78464127448bfc8ac481e
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-11-08 00:08:07 +0545

    refactor: Remove unused dependencies and update component layout

--- src/components/CommandInfo.tsx
+++ src/components/CommandInfo.tsx
@@ -1,22 +1,13 @@
-import { Box, Text, useFocus } from "ink";
-import Link from "ink-link";
-import React from "react";
 import { getCommandInfo } from "@utils/index.js";
+import { Box, Text } from "ink";
+import React from "react";
 
 export function CommandInfo() {
-	useFocus({ isActive: false });
 	return (
-		<Box width="100%" minHeight={5} justifyContent="space-between" flexWrap="wrap">
-			<Box flexGrow={3}>
-				<Text color="blue" bold>
-					{getCommandInfo()}
-				</Text>
-			</Box>
-			<Box flexGrow={1}>
-				<Link url="https://github.com/rubiin/asdfui">
-					<Text color="magenta">Github</Text>
-				</Link>
-			</Box>
+		<Box width="100%" minHeight={5} marginTop={1} justifyContent="center" flexWrap="wrap">
+			<Text color="blue" bold>
+				{getCommandInfo()}
+			</Text>
 		</Box>
 	);
 }
