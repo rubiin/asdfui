ca32c561dd76ba462f35c0648ce898563b2abb2c
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-11-24 21:33:51 +0545

    feat: Remove unused exports

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./App.js";
-export * from "./cli.js";
-export * from "./components/index.js";
-export * from "./panes/index.js";
-export * from "./stores/index.js";
-export * from "./utils/types.js";
-export * from "./utils/index.js";
